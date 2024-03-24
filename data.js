bc1d6ece168fe2d265d7156eb197e82140ff7afa
WelGeen <90037571+WelGeen@users.noreply.github.com>
2024-03-24 22:18:49 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,385 @@
+var game = {
+  "kwartetspel": [
+	//// Research pattern navigator ////	
+	{
+      "categorie": "Research patterns",
+      "sub_categorie": "",
+      "logo": "logo.svg",
+      "color": "rgb(197, 70, 179)",
+      "catcolor": "rgb(63, 2, 138)",
+      "kaarten": [
+		{
+          "kaart": "Clarify focus and scope",
+          "image": "clarify-focus-and-scope.webp"
+        },
+		{
+          "kaart": "Choosing fitting technology",
+          "image": "choose-fitting-technology.webp"
+        },
+		{
+          "kaart": "Solidify",
+          "image": "solidify.webp"
+        },
+		{
+          "kaart": "Realise as required",
+          "image": "realise-as-required.webp"
+        }
+      ]
+    }, /// aangepast Research patterns v1.0
+    {
+      "categorie": "learn from experts",
+      "sub_categorie": "Library",
+      "logo": "icon-library-nobg.svg",
+      "color": "rgb(160, 200, 65)",
+	  "catcolor": "rgb(70, 162, 94)",
+      "kaarten": [
+		{
+          "kaart": "Best good and bad practices",
+          "image": "lib-best-good-and-bad-practices.png"
+        },
+        {
+          "kaart": "Community research",
+          "image": "community-research.webp"
+        },
+        {
+          "kaart": "Design pattern research",
+          "image": "lib-design-pattern-search.png"
+        },
+        {
+          "kaart": "Expert interview",
+          "image": "lib-expert-interview.png"
+        }
+      ]
+    },
+    {
+      "categorie": "learn from the market",
+	  "sub_categorie": "Library",
+	  "logo": "icon-library-nobg.svg",
+      "color": "rgb(160, 200, 65)",
+	  "catcolor": "rgb(5, 100, 96)",
+      "kaarten": [
+		{
+          "kaart": "SWOT analysis",
+          "image": "swot-analysis.webp"
+        },
+		
+        {
+          "kaart": "Available product analysis",
+          "image": "available-product-analysis.webp"
+        },
+        {
+          "kaart": "Competitive analysis",
+          "image": "lib-competitive-analysis-anim.gif"
+        },
+        {
+          "kaart": "Trend analysis",
+          "image": "lib-trend-analysis.png"
+        }
+      ]
+    },/// aangepast Library  v1.0
+
+    {
+      "categorie": "Talk to Users",
+	  "sub_categorie": "Field",
+	  "logo": "icon-field-nobg.svg",
+      "color": "rgb(93, 187, 178)",
+	  "catcolor": "rgb(225, 8, 148)",
+      "kaarten": [
+        {
+          "kaart": "Explore user requirements",
+          "image": "explore-user-requirements.webp"
+        },
+        {
+          "kaart": "Focus group",
+          "image": "field-focus-group.png"
+        },
+        {
+          "kaart": "Interview",
+          "image": "field-interview.png"
+        },
+        {
+          "kaart": "Observation",
+          "image": "field-observation.png"
+        }
+      ]
+    },
+    {
+      "categorie": "analyse",
+	  "sub_categorie": "Field",
+	  "logo": "icon-field-nobg.svg",
+      "color": "rgb(93, 187, 178)",
+	  "catcolor": "rgb(89, 125, 248)",
+      "kaarten": [
+        {
+          "kaart": "Domain modelling",
+          "image": "domain-modelling.webp"
+        },
+        {
+          "kaart": "Card sorting",
+          "image": "field-card-sorting.png"
+        },
+        {
+          "kaart": "Document analysis",
+          "image": "document-analysis.webp"
+        },
+        {
+          "kaart": "Exploratory data analysis (ML)",
+          "image": "exploratory-data-analysis-ml.png"
+        }
+      ]
+    },
+	/// aangepast Field  v1.0
+    {
+      "categorie": "Testing Machine Learning",
+	  "sub_categorie": "Lab",
+	  "logo": "icon-lab-nobg.svg",
+      "color": "rgb(161, 119, 238)",
+	  "catcolor": "rgb(124, 148, 49)",
+      "kaarten": [
+        {
+          "kaart": "Data quality check",
+          "image": "lab-data-quality-check-ml.webp"
+        },
+        {
+          "kaart": "Model evaluation",
+          "image": "lab-model-evaluation-ml.png"
+        },
+        {
+          "kaart": "Model validation",
+          "image": "lab-model-validation-ml.png"
+        },
+		{
+          "kaart": "Data analytics",
+          "image": "lab-web-analytics.png"
+        }
+      ]
+    },
+    {
+      "categorie": "Software testing",
+	  "sub_categorie": "Lab",
+	  "logo": "icon-lab-nobg.svg",
+	  "color": "rgb(161, 119, 238)",
+	  "catcolor": "rgb(109, 157, 174)",
+      "kaarten": [
+        {
+          "kaart": "Hardware validation",
+          "image": "hardware-validation.webp"
+        },
+        {
+          "kaart": "Security test",
+          "image": "lab-security-test.png"
+        },
+        {
+          "kaart": "System test",
+          "image": "system-test.webp"
+        },
+        {
+          "kaart": "Unit test",
+          "image": "unit-test.webp"
+        }
+      ]
+    },
+    {
+      "categorie": "User testen",
+	  "sub_categorie": "Lab",
+	  "logo": "icon-lab-nobg.svg",
+      "color": "rgb(161, 119, 238)",
+	  "catcolor": "rgb(225, 77, 111)",
+      "kaarten": [
+        {
+          "kaart": "Usability testing",
+          "image": "lab-usability-testing-anim.gif"
+        },
+        {
+          "kaart": "Non-functional test",
+          "image": "non-functional-test.webp"
+        },
+        {
+          "kaart": "A/B testing",
+          "image": "lab-AB-testing.png"
+        },
+        {
+          "kaart": "Thinking aloud",
+          "image": "lab-thinking-aloud.png"
+        }
+      ]
+    },/// aangepast Lab  v1.0
+    {
+      "categorie": "to be certain",
+	  "sub_categorie": "Showroom",
+	  "logo": "icon-showroom-nobg.svg",
+      "color": "rgb(249, 206, 88)",
+	  "catcolor": "rgb(125, 107, 11)",
+      "kaarten": [
+        {
+          "kaart": "Benchmark test",
+          "image": "benchmark-test.webp"
+        },
+        {
+          "kaart": "Ethical check",
+          "image": "show-ethical-check.png"
+        },
+        {
+          "kaart": "Guideline conformity analysis",
+          "image": "show-guideline-conformity-analysis.png"
+        },
+        {
+          "kaart": "Static program analysis",
+          "image": "static-program-analysis.webp"
+        }
+      ]
+    },
+    {
+      "categorie": "Demonstrate",
+	  "sub_categorie": "Showroom",
+	  "logo": "icon-showroom-nobg.svg",
+      "color": "rgb(249, 206, 88)",
+	  "catcolor": "rgb(179, 31, 27)",
+      "kaarten": [
+        {
+          "kaart": "Pitch",
+          "image": "show-pitch.png"
+        },
+        {
+          "kaart": "Product review",
+          "image": "show-quality-review.png"
+        },
+        {
+          "kaart": "Peer review",
+          "image": "show-peer-review.png"
+        },
+        {
+          "kaart": "Heuristic Evaluation",
+          "image": "show-heuristic-evaluation.png"
+        }
+      ]
+    },/// aangepast Showroom  v1.0
+    {
+      "categorie": "Explore ideas",
+	  "sub_categorie": "Workshop",
+	  "logo": "icon-workshop-nobg.svg",
+      "color": "rgb(21, 177, 209)",
+	  "catcolor": "rgb(24, 47, 59)",
+      "kaarten": [
+        {
+          "kaart": "Brainstorm",
+          "image": "brainstorm.webp"
+        },
+		{
+          "kaart": "Prototyping",
+          "image": "work-prototyping.png"
+        },
+        {
+          "kaart": "Hackathon",
+          "image": "work-hackathon.png"
+        },
+		{
+          "kaart": "Co-creation",
+          "image": "work-co-creation.png"
+        }
+      ]
+    },
+    {
+      "categorie": "Design solutions",
+	  "sub_categorie": "Workshop",
+	  "logo": "icon-workshop-nobg.svg",
+      "color": "rgb(21, 177, 209)",
+	  "catcolor": "rgb(60, 103, 175)",
+      "kaarten": [
+        {
+          "kaart": "Decomposition",
+          "image": "decomposition.webp"
+        },
+        {
+          "kaart": "IT architecture sketching",
+          "image": "it-architecture-sketching.webp"
+        },
+		{
+          "kaart": "Requirements prioritization",
+          "image": "requirements-prioritization.webp"
+        },
+		{
+          "kaart": "Code review",
+          "image": "code-review.webp"
+        }
+      ]
+    },
+    {
+      "categorie": "explore the Context",
+	  "sub_categorie": "Workshop",
+	  "logo": "icon-workshop-nobg.svg",
+      "color": "rgb(21, 177, 209)",
+	  "catcolor": "rgb(254, 131, 9)",
+      "kaarten": [
+		{
+          "kaart": "Multi-criteria decision making",
+          "image": "multi-criteria-decision-making.webp"
+        },
+        {
+          "kaart": "Business case exploration",
+          "image": "business-case-exploration.webp"
+        },
+        {
+          "kaart": "Root cause analysis",
+          "image": "root-cause-analysis.webp"
+        },
+		{
+          "kaart": "Gap analysis",
+          "image": "gap-analysis.webp"
+        }
+      ]
+    },/// aangepast Workshop  v1.0
+    {
+      "categorie": "condense your insights",
+	  "sub_categorie": "Stepping Stones",
+	  "logo": "icon-stepping-stones-nobg.svg",
+      "color": "rgb(251, 65, 136)",
+	  "catcolor": "rgb(157, 147, 12)",
+      "kaarten": [
+        {
+          "kaart": "Business model canvas",
+          "image": "step-business-model-canvas.png"
+        },
+        {
+          "kaart": "Comparison chart",
+          "image": "step-comparison-chart.png"
+        },
+		{
+          "kaart": "Customer journey",
+          "image": "step-customer-journey.png"
+        },
+
+        {
+          "kaart": "Design specification",
+          "image": "step-design-specification.png"
+        }
+      ]
+    },
+    {
+      "categorie": "to communicate",
+	  "sub_categorie": "Stepping Stones",
+      "color": "rgb(251, 65, 136)",
+	  "logo": "icon-stepping-stones-nobg.svg",
+	  "catcolor": "rgb(27, 69, 136)",
+      "kaarten": [
+		{
+          "kaart": "Concept",
+          "image": "step-concept.png"
+        },
+        {
+          "kaart": "Empathy Map",
+          "image": "step-empathy-map.png"
+        },
+        {
+          "kaart": "Inspiration Wall",
+          "image": "step-inspiration-wall.png"
+        },
+        {
+          "kaart": "Mood Board",
+          "image": "step-moodboard.png"
+        }
+      ]
+    }/// aangepast Stepping Stones  v1.0
+
+  ]
+}
